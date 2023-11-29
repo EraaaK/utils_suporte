e8f3e55891d0940ec6cc3542a8ff75708e4634f7
tn-russo <52663832+tn-russo@users.noreply.github.com>
2023-11-29 20:15:09 +0000

    Estrutura replit ajustada. Código funcional na url

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, make_response, request, render_template, redirect, url_for, flash, jsonify, send_file, Response
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
